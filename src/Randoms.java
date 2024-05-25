0a2527629e0784289817386576ed79d3e7aab486
JavaJedi <aleksk170400@gmail.com>
2024-05-26 03:27:17 +0500

    Initial commit

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Randoms {
+import java.util.*;
+
+public class Randoms implements Iterable<Integer> {
+    protected int min, max;
+    protected Iterator<Integer> randomIterator;
+
+    public Randoms(int min, int max) {
+        randomIterator = new Random().ints(min, (max + 1)).iterator();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return randomIterator.next();
+            }
+        };
+    }
 }
